392331c0d75cb9049c308df5e97edb04b8672369
sadlerb <sadlerb22@gmail.com>
2023-05-21 17:14:18 -0500

    moved files

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
